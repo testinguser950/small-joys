d359d15dab66749c610fe4f9e619b954a4f53ef7
Syron Ng <syron.ng@gmail.com>
2026-03-27 17:56:48 +0800

    Added report text to comments v2

--- app/page.tsx
+++ app/page.tsx
@@ -129,6 +129,9 @@ function ReportCommentButton({ commentId }: { commentId: string }) {
         <path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1z"/>
         <line x1="4" y1="22" x2="4" y2="15"/>
       </svg>
+      <span style={{ fontFamily: "'DM Sans', sans-serif", fontSize: "0.65rem", fontWeight: 400 }}>
+        {reported ? "reported" : "report"}
+      </span>
     </button>
   );
 }
